2063d9018311c460bac995c1283a53449d95fc5a
Irakli Nadareishvili <irakli@gmail.com>
2012-02-12 01:14:31 -0500

    first test passing

--- tests/ZaphpaRestTest.php
+++ tests/ZaphpaRestTest.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once (__DIR__ . '/ZaphpaTestCase.class.php');
+require_once (__DIR__ . '/ZaphpaRestClient.class.php');
 
 class ZaphpaRestTest extends ZaphpaTestCase {
 
@@ -8,10 +9,12 @@ class ZaphpaRestTest extends ZaphpaTestCase {
   
   public function setUp() {
     parent::setUp();
-    //$this->rest_client = SetteeRestClient::get_instance($this->db_url);
+    $this->rest_client = ZaphpaRestClient::get_instance($this->server_url);
   }
 
   public function test_get_entity() {
-		echo "success " . $this->server;
+		$resp = $this->rest_client->http_get('users/1');
+		$this->assertEquals(1, print_r($resp->decoded->params->id, true), "Entity Get Test: id check");
 	}  
+
 }
