a60fe299e68bf2282b216229013c001e8a7672e0
Irakli Nadareishvili <irakli@gmail.com>
2012-05-22 12:27:54 -0400

    improving auto class loader for plugins

--- autoloader.php
+++ autoloader.php
@@ -3,6 +3,18 @@
 spl_autoload_register ('zaphpa_autoloader');
 
 function zaphpa_autoloader($classname) {
+  static $already_checked = array();
+  
+  //Marginal perf. optimization: this is faster than running file_exists() + require_once() repeatedly
+  if (array_key_exists($classname, $already_checked)) {
+    return;
+  }
+  
+  $already_checked[$classname] = true;
+  
   $pathname = __DIR__ . '/plugins/' . $classname . '.class.php';
-  require_once($pathname);   
+  if (file_exists($pathname)) {
+    
+    require_once($pathname);   
+  }
 }
