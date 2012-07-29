28e6726c7d0cbf029000a3f23763d7e4a1c53f08
Irakli Nadareishvili <irakli@gmail.com>
2012-07-29 00:47:12 -0400

    moved all tests to using restagent.php

--- tests/ZaphpaRestTest.php
+++ tests/ZaphpaRestTest.php
@@ -2,63 +2,60 @@
 
 require_once (__DIR__ . '/ZaphpaTestCase.class.php');
 require_once (__DIR__ . '/ZaphpaRestClient.class.php');
+require_once (__DIR__ . '/restagent.lib.php');
 
 /**
  * @file
  * Functional tests for basic endpoint processing
  */
 class ZaphpaRestTest extends ZaphpaTestCase {
-  private $rest_client;
+  private $request;
   
   public function setUp() {
     parent::setUp();
-    $this->rest_client = ZaphpaRestClient::get_instance($this->server_url);
+    $this->request = new \restagent\Request($this->server_url);
   }
 
   public function test_pattern_num_single() {
     try {
-      $resp = $this->rest_client->get('users/1');
+      $resp = (object) $this->request->get('/users/1');
+      $resp->decoded = json_decode($resp->data);
       $this->assertEquals(1, $resp->decoded->params->id, '{1} was not parsed correctly.');
     } catch (ZaphpaRestClientException $ex) {
       $this->fail('Request failed when numeric characters should have passed.');
     }
-    
-    try {
-      $resp = $this->rest_client->get('users/alpha');
-    } catch (ZaphpaRestClientException $ex) {
-      return;
-    }
-     
-    $this->fail('Alpha characters were parsed when a numeric arg was expected.');
+
+    $resp = $this->request->get('/users/alpha');
+    $this->assertEquals(404, $resp['code'], 'Alpha characters were parsed when a numeric arg was expected.');    
   }  
 
   public function test_pattern_alpha_single() {
     try {
-      $resp = $this->rest_client->get('tags/alpha');
+      $resp = (object) $this->request->get('/tags/alpha');
+      $resp->decoded = json_decode($resp->data);
+      
       $this->assertEquals('alpha', $resp->decoded->params->id, '{alpha} was not parsed correctly.');
     } catch (ZaphpaRestClientException $ex) {
       $this->fail('Request failed when alpha-num chars should have passed.');
     }
 
     try {
-      $resp = $this->rest_client->get('tags/234234');
-      $this->assertEquals('234234', $resp->decoded->params->id, '{234234} was not parsed correctly.');
+      $resp = (object) $this->request->get('/tags/234234');
+      $resp->decoded = json_decode($resp->data);      
+      $this->assertEquals('234234', $resp->decoded->params->id, 'URI param {234234} was not parsed correctly.');
     } catch (ZaphpaRestClientException $ex) {
-      $this->fail('Request failed when alpha-num chars should have passed.');
+      $this->fail('Request failed when numeric chars should have passed.');
     }
 
-    try {
-      $this->rest_client->get('tags/aa$%`^sff|_');
-    } catch (ZaphpaRestClientException $ex) {
-      return;
-    }
-    
-    $this->fail('Special characters were parsed when an alpha-num arg was expected.');
+    $resp = (object) $this->request->get('/tags/aa[]&');
+    $this->assertEquals('404', $resp->code, 'Special characters were parsed when an alpha-num arg was expected.');
   }
 
   public function test_num_and_alpha_two_params() {
     try {
-      $resp = $this->rest_client->get('users/1234/books/shakespear');
+      $resp = (object) $this->request->get('/users/1234/books/shakespear');
+      $resp->decoded = json_decode($resp->data);      
+          
       $this->assertEquals('1234', $resp->decoded->params->user_id);
       $this->assertEquals('shakespear', $resp->decoded->params->book_id);
     } catch (ZaphpaRestClientException $ex) {
@@ -66,78 +63,76 @@ class ZaphpaRestTest extends ZaphpaTestCase {
     }
 
     try {
-      $resp = $this->rest_client->get('users/1234/books/35345');
+      $resp = (object) $this->request->get('/users/1234/books/35345');
+      $resp->decoded = json_decode($resp->data);      
+      
       $this->assertEquals('1234', $resp->decoded->params->user_id);
       $this->assertEquals('35345', $resp->decoded->params->book_id);
     } catch (ZaphpaRestClientException $ex) {
       $this->fail('Request failed when both args should have succeeded.');
     }
 
-    try {
-      $this->rest_client->get('users/asfksalfjk/books/35345');
-    } catch (ZaphpaRestClientException $ex) {
-      return;
-    }
-
-    $this->fail('Alpha characters were parsed when a numeric arg was expected');
+    $resp = (object) $this->request->get('/users/asfksalfjk/books/35345');  
+    $this->assertEquals(404, $resp->code, 'Alpha characters were parsed when a numeric arg was expected');
   }
   
   public function test_middleware() {
-    try {
-      $resp = $this->rest_client->get('middlewaretest/777');
+    try {      
+      $resp = (object) $this->request->get('/middlewaretest/777');
+      $resp->decoded = json_decode($resp->data); 
+      
       $this->assertEquals(777, $resp->decoded->params->mid, 'Middleware response test');
-    } catch (ZaphpaRestClientException $ex) {
-      $this->fail('Middleware test: response test should have passed.');
-    }   
-    
-    try {
-      $resp = $this->rest_client->get('middlewaretest/777');
       $this->assertEquals('foo', $resp->decoded->params->bogus, 'Middleware preroute test');
-    } catch (ZaphpaRestClientException $ex) {
-      $this->fail('Middleware test: preroute attachment of bogus param should have passed.');
-    }   
-
-    try {
-      $resp = $this->rest_client->get('middlewaretest/777');
       $this->assertEquals('2.0', $resp->decoded->version, 'Middleware prerender test');
+      
+      
     } catch (ZaphpaRestClientException $ex) {
-      $this->fail('Middleware test: prerender replacement of version should have passed.');
+      $this->fail('Middleware test: middleware routing test should have passed.');
     }   
   }
 
   public function test_scoped_middleware() {
-    $resp = $this->rest_client->get('foo');
-    $this->assertEquals('success', $resp->decoded->scopeModification, 'Scoped middleware test: Expected middleware to run and modify response for GET.');
-      
-    /**
-    $resp = $this->rest_client->put('foo');
-    $this->assertEquals('success', $resp->decoded->scopeModification, 'Scoped middleware test: Expected middleware to run and modify response for PUT.');
-    **/
+    $resp = (object) $this->request->get('/foo');
+    $resp->decoded = json_decode($resp->data); 
     
-    $resp = $this->rest_client->get('foo/bar');
+    $this->assertEquals('success', $resp->decoded->scopeModification, 'Scoped middleware test: Expected middleware to run and modify response for GET.');
+    
+    $resp = (object) $this->request
+                          ->data("hobby", "programming")
+                          ->put('/foo');
+    $resp->decoded = json_decode($resp->data);     
+    $this->assertEquals('success', $resp->decoded->scopeModification, 'Scoped middleware test: Expected middleware to run and modify response for PUT.');
+    
+    $resp = (object) $this->request->get('/foo/bar');
+    $resp->decoded = json_decode($resp->data);     
     $this->assertEquals('GET', $resp->decoded->method, 'Scoped middleware test: Expected middleware not to run.');
 
-    /**
-    $resp = $this->rest_client->put('foo/bar');
-    $this->assertEquals('success', $resp->decoded->scopeModification, 'Scoped middleware test: Expected middleware to run and modify response.');
-    **/
-    
+    $resp = (object) $this->request
+                          ->data("hobby", "programming")
+                          ->put('/foo/bar');
+    $resp->decoded = json_decode($resp->data);     
+    $this->assertEquals('success', $resp->decoded->scopeModification, 'Scoped middleware test: Expected middleware to run and modify response.');    
   }
 
   public function test_middleware_autodoc() {
     try {
-      $resp = $this->rest_client->get('testapidocs');
+      $resp = (object) $this->request->get('/testapidocs');
+      $resp->decoded = json_decode($resp->data);     
+      $this->assertEquals(200, $resp->code, 'Middleware test: auto documentator response test should pass.');
     } catch (ZaphpaRestClientException $ex) {
       $this->fail('Middleware test: auto documentator response test should have passed.');
     }
   }
 
   public function test_middleware_cors() {
-    $resp = $this->rest_client->get('users');
+    $resp = (object) $this->request->get('/users');
+    $resp->decoded = json_decode($resp->data);         
+
     $this->assertArrayHasKey('Access-Control-Allow-Origin', $resp->headers, 'CORS test: expected proper CORS headers to be set.');
     $this->assertEquals('*', $resp->headers['Access-Control-Allow-Origin'], 'CORS test: expected proper CORS headers to be set.');
 
-    $resp = $this->rest_client->get('users/123');
+    $resp = (object) $this->request->get('/users/123');
+    $resp->decoded = json_decode($resp->data);             
     $this->assertArrayNotHasKey('Access-Control-Allow-Origin', $resp->headers, 'CORS test: expected CORS headers not to be set.');
   }
 }
