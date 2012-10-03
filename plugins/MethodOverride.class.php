cd0e18365c8bcd55d2f5edcee5663b8ca0e4761d
Irakli Nadareishvili <irakli@gmail.com>
2012-10-03 08:57:44 -0400

    added method override

--- plugins/MethodOverride.class.php
+++ plugins/MethodOverride.class.php
@@ -3,7 +3,8 @@
 class MethodOverride extends Zaphpa_Middleware {  
   
   function preprocess(&$router) {
-    if (!empty($_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE'])) {
+    if (!empty($_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE']) &&
+        Zaphpa_Router::getRequestMethod() == "post") {
       $_SERVER['REQUEST_METHOD'] = $_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE'];
     }
   }
